2e66451f1e9828fb5c6810b10d2f88396590c585
Gregory McIntyre <blue.puyo@gmail.com>
2011-02-03 09:05:50 +1100

    Even cleaner javascript. Yay.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,52 +34,78 @@
     }
   };
 
-  DrawingClass = {
-    drawBrush: function(targetContext, data, tx, ty, col) {
-      var self = this;
-      targetContext.fillStyle = col;
+  Drawing = function(args) {
+    this.initialize(args);
+    return true;
+  };
 
-      tx = Math.floor(tx);
-      ty = Math.floor(ty);
-      for (var y = 0; y < self._brushHeight; ++y) {
-        for (var x = 0; x < self._brushWidth; ++x) {
-          if (data[y*self._brushWidth + x] === 1) {
-            var drawX = (tx + x - self._brushWidth/2 - 1)*self.options.scale;
-            var drawY = (ty + y - self._brushHeight/2 - 1)*self.options.scale;
-            targetContext.fillRect(drawX, drawY, self.options.scale, self.options.scale);
-          }
-        }
-      }
-      // debugging:
-      //targetContext.fillStyle = 'red';
-      //targetContext.fillRect(tx*scale, ty*scale, 1, 1);
-    },
-
-    setLastPos: function(event) {
+  Drawing.prototype = {
+    initialize: function(args) {
       var self = this;
-      lastX = Math.floor(event.offsetX / self.options.scale);
-      lastY = Math.floor(event.offsetY / self.options.scale);
-    },
 
-    draw: function(event) {
-      var self = this;
-      if (self._down) {
-        var x = Math.floor(event.offsetX / self.options.scale);
-        var y = Math.floor(event.offsetY / self.options.scale);
-        if (x >= self.width) x = self.width - 1;
-        if (y >= self.height) y = self.height - 1;
-        if (lastX === null)
-          lastX = x;
-        if (lastY === null)
-          lastY = y;
-        bresenham(lastX, lastY, x, y, function(i, j) { self.drawBrush(self._context, self._brush, i, j, self.options.brushCol); });
-        lastX = x;
-        lastY = y;
-      }
-    },
+      var defaults = {
+        selector: '#drawing',
+        scale: 3,
+        width: 128,
+        height: 128
+      };
+      self.options = $.extend({}, defaults, args || {});
 
-    setupDOM: function() {
-      var self = this;
+      self.options.pixelWidth = self.options.width*self.options.scale;
+      self.options.pixelHeight = self.options.height*self.options.scale;
+      self.options.brushCol = "black";
+
+      self._brushes = {
+        pixel: [
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,1,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0],
+        circle: [
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,1,1,0,0,0,
+          0,0,1,1,1,1,0,0,
+          0,0,1,1,1,1,0,0,
+          0,0,0,1,1,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0],
+        largeCircle: [
+          0,0,0,1,1,0,0,0,
+          0,1,1,1,1,1,1,0,
+          0,1,1,1,1,1,1,0,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          0,1,1,1,1,1,1,0,
+          0,1,1,1,1,1,1,0,
+          0,0,0,1,1,0,0,0],
+        largeSquare: [
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1]};
+
+      self._brush = self._brushes.pixel;
+      self._down = false;
+      self._lastX = null;
+      self._lastY = null;
+      self._brushWidth = 8;
+      self._brushHeight = 8;
+
+      // Setup DOM
+
+      self.options.element = $(self.options.selector);
+      if (!self.options.element) return;
+      self._context = self.options.element[0].getContext('2d');
+
       self.options.element.attr('width', self.options.pixelWidth).attr('height', self.options.pixelWidth);
 
       self.options.element.mousedown(function(event){
@@ -143,84 +169,53 @@
       self.drawBrush($('.brush.black.pixel canvas')[0].getContext('2d'), self._brushes.pixel, 5, 5, "black");
     },
 
-    initialize: function(args) {
+    drawBrush: function(targetContext, data, tx, ty, col) {
       var self = this;
+      targetContext.fillStyle = col;
 
-      var defaults = {
-        selector: '#drawing',
-        scale: 3,
-        width: 128,
-        height: 128
-      };
-      self.options = $.extend({}, defaults, args || {});
+      tx = Math.floor(tx);
+      ty = Math.floor(ty);
+      for (var y = 0; y < self._brushHeight; ++y) {
+        for (var x = 0; x < self._brushWidth; ++x) {
+          if (data[y*self._brushWidth + x] === 1) {
+            var drawX = (tx + x - self._brushWidth/2 - 1)*self.options.scale;
+            var drawY = (ty + y - self._brushHeight/2 - 1)*self.options.scale;
+            targetContext.fillRect(drawX, drawY, self.options.scale, self.options.scale);
+          }
+        }
+      }
+      // debugging:
+      //targetContext.fillStyle = 'red';
+      //targetContext.fillRect(tx*scale, ty*scale, 1, 1);
+    },
 
-      self.options.element = $(self.options.selector);
-      if (!self.options.element) return;
+    setLastPos: function(event) {
+      var self = this;
+      lastX = Math.floor(event.offsetX / self.options.scale);
+      lastY = Math.floor(event.offsetY / self.options.scale);
+    },
 
-      self.options.pixelWidth = self.options.width*self.options.scale;
-      self.options.pixelHeight = self.options.height*self.options.scale;
-      self.options.brushCol = "black";
-
-      self._context = self.options.element[0].getContext('2d');
-
-      self._brushes = {
-        pixel: [
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,1,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0],
-        circle: [
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,1,1,0,0,0,
-          0,0,1,1,1,1,0,0,
-          0,0,1,1,1,1,0,0,
-          0,0,0,1,1,0,0,0,
-          0,0,0,0,0,0,0,0,
-          0,0,0,0,0,0,0,0],
-        largeCircle: [
-          0,0,0,1,1,0,0,0,
-          0,1,1,1,1,1,1,0,
-          0,1,1,1,1,1,1,0,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          0,1,1,1,1,1,1,0,
-          0,1,1,1,1,1,1,0,
-          0,0,0,1,1,0,0,0],
-        largeSquare: [
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1,
-          1,1,1,1,1,1,1,1]};
-
-      self._brush = self._brushes.pixel;
-      self._down = false;
-      self._lastX = null;
-      self._lastY = null;
-      self._brushWidth = 8;
-      self._brushHeight = 8;
-
-      self.setupDOM();
+    draw: function(event) {
+      var self = this;
+      if (self._down) {
+        var x = Math.floor(event.offsetX / self.options.scale);
+        var y = Math.floor(event.offsetY / self.options.scale);
+        if (x >= self.width) x = self.width - 1;
+        if (y >= self.height) y = self.height - 1;
+        if (lastX === null)
+          lastX = x;
+        if (lastY === null)
+          lastY = y;
+        bresenham(lastX, lastY, x, y, function(i, j) { self.drawBrush(self._context, self._brush, i, j, self.options.brushCol); });
+        lastX = x;
+        lastY = y;
+      }
     }
-  };
 
-  Drawing = function(args) {
-    this.__proto__ = DrawingClass;
-    this.initialize(args);
-    return true;
   };
 
 })(jQuery, document);
 
 $(document).ready(function(){
   var d = new Drawing({ domElem: $('#drawarea'), scale: 3, width: 128, height: 128 });
-  console.log(d);
 });
