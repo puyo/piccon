521b39d0cc052c446164d44f4ec40ee06a089563
Gregory McIntyre <blue.puyo@gmail.com>
2011-02-03 08:59:43 +1100

    Tidy up and jslint my javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,80 +34,9 @@
     }
   };
 
-  Drawing = function(args) {
-    var self = this;
-
-    var defaults = {
-      selector: '#drawing',
-      scale: 3,
-      width: 128,
-      height: 128,
-    };
-
-    self.options = $.extend({}, defaults, args || {});
-
-    self.options.element = $(self.options.selector);
-    if (!self.options.element) return;
-
-    self.options.pixelWidth = self.options.width*self.options.scale;
-    self.options.pixelHeight = self.options.height*self.options.scale;
-    self.options.brushCol = "black";
-
-    self.options.element.attr('width', self.options.pixelWidth).attr('height', self.options.pixelWidth);
-    self._context = self.options.element[0].getContext('2d');
-    //context.drawImage(self.domElem, 0, 0, self.pixelWidth, self.pixelHeight, 0, 0, width, height);
-
-    self._brushes = {
-      pixel: [
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,1,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-      ],
-      circle: [
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,1,1,0,0,0,
-        0,0,1,1,1,1,0,0,
-        0,0,1,1,1,1,0,0,
-        0,0,0,1,1,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-      ],
-      largeCircle: [
-        0,0,0,1,1,0,0,0,
-        0,1,1,1,1,1,1,0,
-        0,1,1,1,1,1,1,0,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        0,1,1,1,1,1,1,0,
-        0,1,1,1,1,1,1,0,
-        0,0,0,1,1,0,0,0,
-      ],
-      largeSquare: [
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-        1,1,1,1,1,1,1,1,
-      ],
-    };
-
-    self._brush = self._brushes.pixel;
-    self._down = false;
-    self._lastX = null;
-    self._lastY = null;
-    self._brushWidth = 8;
-    self._brushHeight = 8;
-
-    self.drawBrush = function(targetContext, data, tx, ty, col) {
+  DrawingClass = {
+    drawBrush: function(targetContext, data, tx, ty, col) {
+      var self = this;
       targetContext.fillStyle = col;
 
       tx = Math.floor(tx);
@@ -124,14 +53,16 @@
       // debugging:
       //targetContext.fillStyle = 'red';
       //targetContext.fillRect(tx*scale, ty*scale, 1, 1);
-    }
+    },
 
-    function setLastPos(event) {
+    setLastPos: function(event) {
+      var self = this;
       lastX = Math.floor(event.offsetX / self.options.scale);
       lastY = Math.floor(event.offsetY / self.options.scale);
-    }
+    },
 
-    function draw(event) {
+    draw: function(event) {
+      var self = this;
       if (self._down) {
         var x = Math.floor(event.offsetX / self.options.scale);
         var y = Math.floor(event.offsetY / self.options.scale);
@@ -145,69 +76,151 @@
         lastX = x;
         lastY = y;
       }
-    }
-    self.options.element.mousedown(function(event){
-      //console.log('mouse down', event);
-      self._down = true;
-      setLastPos(event);
-      draw(event);
-      return false;
-    }).mouseup(function(event){
-      //console.log('mouse up', event);
-      self._down = false;
-    }).mouseenter(function(event){
-      //console.log('mouse enter', event);
-      setLastPos(event);
-      draw(event);
-    }).mouseleave(function(event){
-      //console.log('mouse leave', event);
-      draw(event);
-    }).mousemove(function(event){
-      //console.log('mouse move', event);
-      draw(event);
-    });
+    },
 
-    $('button.pixel').click(function(){
-      self._brush = self._brushes.pixel;
-    });
-    $('button.black').click(function(){
+    setupDOM: function() {
+      var self = this;
+      self.options.element.attr('width', self.options.pixelWidth).attr('height', self.options.pixelWidth);
+
+      self.options.element.mousedown(function(event){
+        //console.log('mouse down', event);
+        self._down = true;
+        self.setLastPos(event);
+        self.draw(event);
+        return false;
+      }).mouseup(function(event){
+        //console.log('mouse up', event);
+        self._down = false;
+      }).mouseenter(function(event){
+        //console.log('mouse enter', event);
+        self.setLastPos(event);
+        self.draw(event);
+      }).mouseleave(function(event){
+        //console.log('mouse leave', event);
+        self.draw(event);
+      }).mousemove(function(event){
+        //console.log('mouse move', event);
+        self.draw(event);
+      });
+
+      $('button.pixel').click(function(){
+        self._brush = self._brushes.pixel;
+      });
+      $('button.black').click(function(){
+        self.options.brushCol = "black";
+      });
+      $('button.white').click(function(){
+        self.options.brushCol = "white";
+      });
+      $('button.circle.small').click(function(){
+        self._brush = self._brushes.circle;
+      });
+      $('button.circle.large').click(function(){
+        self._brush = self._brushes.largeCircle;
+      });
+      $('button.square.large').click(function(){
+        self._brush = self._brushes.largeSquare;
+      });
+      $('button.black.clear').click(function(){
+        self._context.fillStyle = "black";
+        self._context.fillRect(0, 0, self.options.pixelWidth, self.options.pixelHeight);
+      });
+      $('button.white.clear').click(function(){
+        self._context.fillStyle = "white";
+        self._context.fillRect(0, 0, self.options.pixelWidth, self.options.pixelHeight);
+      });
+
+      // icons on buttons
+      $('.brush').html("<canvas width="+self._brushWidth*self.options.scale+" height="+self._brushHeight*self.options.scale+"></canvas>");
+      $('button.clear').text("CLR");
+      self.drawBrush($('.brush.black.circle.small canvas')[0].getContext('2d'), self._brushes.circle, 5, 5, "black");
+      self.drawBrush($('.brush.black.circle.large canvas')[0].getContext('2d'), self._brushes.largeCircle, 5, 5, "black");
+      self.drawBrush($('.brush.white.circle.large canvas')[0].getContext('2d'), self._brushes.largeCircle, 5, 5, "white");
+      self.drawBrush($('.brush.white.circle.small canvas')[0].getContext('2d'), self._brushes.circle, 5, 5, "white");
+      self.drawBrush($('.brush.white.square.large canvas')[0].getContext('2d'), self._brushes.largeSquare, 5, 5, "white");
+      self.drawBrush($('.brush.black.square.large canvas')[0].getContext('2d'), self._brushes.largeSquare, 5, 5, "black");
+      self.drawBrush($('.brush.white.pixel canvas')[0].getContext('2d'), self._brushes.pixel, 5, 5, "white");
+      self.drawBrush($('.brush.black.pixel canvas')[0].getContext('2d'), self._brushes.pixel, 5, 5, "black");
+    },
+
+    initialize: function(args) {
+      var self = this;
+
+      var defaults = {
+        selector: '#drawing',
+        scale: 3,
+        width: 128,
+        height: 128
+      };
+      self.options = $.extend({}, defaults, args || {});
+
+      self.options.element = $(self.options.selector);
+      if (!self.options.element) return;
+
+      self.options.pixelWidth = self.options.width*self.options.scale;
+      self.options.pixelHeight = self.options.height*self.options.scale;
       self.options.brushCol = "black";
-    });
-    $('button.white').click(function(){
-      self.options.brushCol = "white";
-    });
-    $('button.circle.small').click(function(){
-      self._brush = self._brushes.circle;
-    });
-    $('button.circle.large').click(function(){
-      self._brush = self._brushes.largeCircle;
-    });
-    $('button.square.large').click(function(){
-      self._brush = self._brushes.largeSquare;
-    });
-    $('button.black.clear').click(function(){
-      self._context.fillStyle = "black";
-      self._context.fillRect(0, 0, self.options.pixelWidth, self.options.pixelHeight);
-    });
-    $('button.white.clear').click(function(){
-      self._context.fillStyle = "white";
-      self._context.fillRect(0, 0, self.options.pixelWidth, self.options.pixelHeight);
-    });
 
-    // icons on buttons
-    $('.brush').html("<canvas width="+self._brushWidth*self.options.scale+" height="+self._brushHeight*self.options.scale+"></canvas>");
-    self.drawBrush($('.brush.black.circle.small canvas')[0].getContext('2d'), self._brushes.circle, 5, 5, "black");
-    self.drawBrush($('.brush.black.circle.large canvas')[0].getContext('2d'), self._brushes.largeCircle, 5, 5, "black");
-    self.drawBrush($('.brush.white.circle.large canvas')[0].getContext('2d'), self._brushes.largeCircle, 5, 5, "white");
-    self.drawBrush($('.brush.white.circle.small canvas')[0].getContext('2d'), self._brushes.circle, 5, 5, "white");
-    self.drawBrush($('.brush.white.square.large canvas')[0].getContext('2d'), self._brushes.largeSquare, 5, 5, "white");
-    self.drawBrush($('.brush.black.square.large canvas')[0].getContext('2d'), self._brushes.largeSquare, 5, 5, "black");
-    self.drawBrush($('.brush.white.pixel canvas')[0].getContext('2d'), self._brushes.pixel, 5, 5, "white");
-    self.drawBrush($('.brush.black.pixel canvas')[0].getContext('2d'), self._brushes.pixel, 5, 5, "black");
+      self._context = self.options.element[0].getContext('2d');
+
+      self._brushes = {
+        pixel: [
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,1,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0],
+        circle: [
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,1,1,0,0,0,
+          0,0,1,1,1,1,0,0,
+          0,0,1,1,1,1,0,0,
+          0,0,0,1,1,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0],
+        largeCircle: [
+          0,0,0,1,1,0,0,0,
+          0,1,1,1,1,1,1,0,
+          0,1,1,1,1,1,1,0,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          0,1,1,1,1,1,1,0,
+          0,1,1,1,1,1,1,0,
+          0,0,0,1,1,0,0,0],
+        largeSquare: [
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1]};
+
+      self._brush = self._brushes.pixel;
+      self._down = false;
+      self._lastX = null;
+      self._lastY = null;
+      self._brushWidth = 8;
+      self._brushHeight = 8;
+
+      self.setupDOM();
+    }
+  };
+
+  Drawing = function(args) {
+    this.__proto__ = DrawingClass;
+    this.initialize(args);
+    return true;
   };
 
 })(jQuery, document);
 
 $(document).ready(function(){
   var d = new Drawing({ domElem: $('#drawarea'), scale: 3, width: 128, height: 128 });
+  console.log(d);
 });
